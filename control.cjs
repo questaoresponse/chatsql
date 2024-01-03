c47c2e09196fd2e06a18c9fe8af93065108b1cff
questaoresponse <eneagonlosamigos@gmail.com>
2024-01-03 18:31:42 -0300

    add

---
+++ control.cjs
@@ -0,0 +1,2 @@
+const {PeerServer}=require("peer");
+const peer=PeerServer({port:9000,path:"/peerjs"});
--- server.cjs
+++ server.cjs
@@ -5,13 +5,20 @@ const socketio=require("socket.io");
 // const socketioClient=require("socket.io-client");
 const cors=require("cors");
 const sqlite3 = require('sqlite3').verbose();
-const app=express();
-const server=http.createServer(app);
 const axios=require("axios");
+const {PeerServer}=require("peer");
 const { createClient }=require("@supabase/supabase-js");
 const supabaseUrl = 'https://bqgcqatezmtdelrvywka.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZ2NxYXRlem10ZGVscnZ5d2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQxNjI5NjcsImV4cCI6MjAxOTczODk2N30.b8YLKt55jYQ-ScUVNkduIO7fmEN2ryTQmBM9nROHtm0';
 const supabase = createClient(supabaseUrl, supabaseKey);
+const app=express();
+const server=http.createServer(app);
+const peer=PeerServer({port:9000,path:"/peerjs/peerjs"});
+// const peerServer = ExpressPeerServer(server, {
+//     debug: true,
+//     path: '/peerjs', // Define o caminho para o servidor de sinalização PeerJS
+// });
+// app.use("/peerjs",peerServer);
 // supabase.from("ai").
 // const sql = postgres(connectionString)
 // const client = new Client({
@@ -96,16 +103,44 @@ const conn=new conn2("db");
 // conn.query("DELETE FROM chat");
 var status={};
 io.on("connection",(socket)=>{
+    var user;
+    var call_id;
     // conn.query("SELECT * FROM msg WHERE usuario=?",[usuario],(err,result)=>{
     //     socket.emit("msg",result);
     // })
     // socket.emit("msg",[{text:"teste"}]);
-    socket.on("infos",(data)=>{
-        socket.user=data;
-        status[socket.user]="online";
+    
+    socket.on("call",async (data)=>{
+        const id=Number(data);
+        const {data:result,error}=await supabase.from("chat").select("usuario1,usuario2").eq("id",id);
+        const usuario2=result[0]["usuario1"]==user ? result[0]["usuario2"] : result[0]["usuario1"];
+        // const id=result[0]["id"];
+        if (status[usuario2]){
+            // socket.join(id);
+            socket.join(usuario2);
+            socket.to(usuario2).emit("call",id);
+            socket.leave(usuario2);
+            // s
+        };
     })
-    socket.on("chat",async (id)=>{
-        const usuario=socket.user;
+    socket.on("infos",async (data)=>{
+        user=data;
+        socket.join(user);
+        status[user]="online";
+        const {data:result,error}=await supabase.from("chat").select("id").or(`usuario1.eq.${user},usuario2.eq.${user}`);
+        if (result.length>0){
+            result.forEach(res=>{
+                const id=res.id;
+                socket.join(id);
+                const size=io.sockets.adapter.rooms.get(id)?.size || 0;
+                size==3 && socket.to(id).emit("status","online");
+                socket.leave(id);
+            })
+        }
+    })
+    socket.on("chat",async (ids)=>{
+        const usuario=user;
+        const id=Number(ids);
         // const resultr=await conn.query("SELECT * FROM chat WHERE id=?",[id]);
         const {data:resultr,error}=await supabase.from("chat").select("*").eq("id",id);
         if (!resultr || resultr.length==0){
@@ -113,35 +148,63 @@ io.on("connection",(socket)=>{
             return;
         } else {
             // const result=await conn.query("SELECT * FROM msg WHERE chat=?",[id]);
-            const {data:result,error}=await supabase.from("msg").select("*").eq("chat",Number(id));
+            const usuario2=resultr[0].usuario1==usuario ? resultr[0].usuario2 : resultr[0].usuario1;
+            await supabase.from("msg").update({visualization:new Date().toISOString()}).eq("chat",Number(id)).eq("usuario",usuario2).eq("visualization",false);
+            const {data:result,error}=await supabase.from("msg").select("*").eq("chat",Number(id)).order("id",{ascending:true});
             socket.emit("msgs",{msgs:result});
-            const o=resultr[0].usuario1==usuario ? resultr[0].usuario2 : resultr[0].usuario1;
-            socket.emit("infos",{status:status[o] || "offline",name:[resultr[0].usuario1,resultr[0].usuario2]});
+            socket.emit("infos",{status:status[usuario2] || "offline",name:[resultr[0].usuario1,resultr[0].usuario2]});
             socket.join(id);
-            socket.to(id).emit("status","online");
         }
     });
     socket.on("quit-chat",async (name)=>{
-        io.to(name).emit("status","offline");
-        socket.leave(name);
+        // io.to(name).emit("status","offline");
+        socket.leave(Number(name));
     });
     socket.on("msg",async (data)=>{
         data.room=Number(data.room);
         const date=new Date().toISOString();
-        socket.to(data.room).emit("msg",{usuario:data.usuario,text:data.text,date:date});
+        // const chat_id=data.chat_id;
         // conn.query("INSERT INTO msg(usuario,chat,text,date) VALUES(?,?,?,?)",[data.usuario,data.room,data.text,date]);
-        await supabase.from("msg").insert({usuario:data.usuario,chat:data.room,text:data.text,date:date});
+        const {data:result,error}=await supabase.from("msg").select("chat_id").eq("chat",data.room).order("id",{ascending:false}).limit(1);
+        const chat_id=result.length > 0 ? Number(result[0]["chat_id"])+1 : 1;
+        socket.to(data.room).emit("msg",{chat_id:chat_id,usuario:data.usuario,text:data.text,date:date});
+        await supabase.from("msg").insert({visualization:false,chat_id:chat_id,usuario:data.usuario,chat:data.room,text:data.text,date:date});
+        // socket.to(data.room).emit("visualization-msg",{date})
     })
     socket.on("isDigiting",(data)=>{
-        socket.to(data.room).emit("status",data.isDigiting ? "digitando..." : status[data.usuario]);
+        status[data.usuario]=data.isDigiting ? "digitando..." : "online";
+        socket.to(data.room).emit("status",status[data.usuario]);
     })
+    socket.on("visualization-msg",async (data)=>{
+        data.chat=Number(data.chat);
+        const visualization=new Date().toISOString();
+        await supabase
+            .from("msg")
+            .update({visualization:visualization})
+            .eq("chat",data.chat)
+            .eq("chat_id",data.chat_id);
+        socket.to(data.chat).emit("visualization-msg",{chat_id:data.chat_id,visualization:visualization});
+    })
+    socket.on("disconnect",async ()=>{
+        delete status[user];
+        const {data:result,error}=await supabase.from("chat").select("id").or(`usuario1.eq.${user},usuario2.eq.${user}`);
+        if (result.length>0){
+            result.forEach(res=>{
+                const id=res.id;
+                socket.join(id);
+                const size=io.sockets.adapter.rooms.get(id)?.size || 0;
+                size==3 && socket.to(id).emit("status","offline");
+                socket.leave(id);
+            })
+        }
+        socket.leave(user);
+    });
 });
 app.post("/login",async (req,res)=>{
     const data=req.body;
     if (data.type=="login"){
         // var result=await conn.query("SELECT * FROM users WHERE email=? AND senha=?",[data.email,data.password]);
-        var { result, error }=await supabase.from("users").select("*").eq("email",data.email).eq("senha",data.password);
-        console.log(result,error);
+        var { data:result, error }=await supabase.from("users").select("*").eq("email",data.email).eq("senha",data.password);
         if (result && result.length>0){
             res.json({result:"true",usuario:result[0].usuario});
         } else {
@@ -175,7 +238,6 @@ app.post("/convite",async (req,res)=>{
             var sort=[usuario,name].sort();
             // result=await conn.query("SELECT * FROM chat WHERE usuario1=? AND usuario2=?",sort);
             var {data:result,error}=await supabase.from("chat").select("*").eq('usuario1',sort[0]).eq('usuario2',sort[1]);
-            console.log(result,error,usuario,name);
             if (result && result.length>0){
                 res.json({result:"exists"});
             } else {
@@ -202,6 +264,24 @@ app.post("/chats",async(req,res)=>{
     const {data:result,error}=await supabase.from("chat").select("*").or(`usuario1.eq.${usuario},usuario2.eq.${usuario}`);
     res.json({chats:result});
 })
+// app.get("/a", async(req,res)=>{
+//     // const {data:result,error}=await supabase.from("msg").select("*");
+//     // for (var i=1;i<result.length+1;i++){
+//     //     const chats=new
+//     //     // const date=result[i-1]["date"];
+//     //     await supabase.from("msg").update({chat_id:chat_id}).eq("chat").eq("id",);
+//     // }
+//     const chs=[1,2,3,4];
+//     for (var ch of chs){
+//         const {data:result,error}=await supabase.from("msg").select("*").eq("chat",ch);
+//         var ui=0;
+//         for (var rs of result){
+//             ui++;
+//             await supabase.from("msg").update({chat_id:ui}).eq("id",rs.id);
+//         }
+//     }
+//     res.send("bom");
+// })
 server.listen("4000",(err)=>{
     console.log("rodando");
 })
